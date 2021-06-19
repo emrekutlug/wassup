b770fb19b597dd90ecb385473772a02d7a3ecede
Emre Kutlug <emrekutlug@Emres-MacBook-Air.local>
2021-06-19 21:37:50 +0300

    first version

---
+++ app/src/androidTest/java/com/emrekutlug/wassup5/MainActivityEspressoTest.java
@@ -0,0 +1,19 @@
+package com.emrekutlug.wassup5;
+
+import androidx.test.filters.LargeTest;
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4ClassRunner.class)
+@LargeTest
+public class MainActivityEspressoTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    // Add instrumentation test here
+}
